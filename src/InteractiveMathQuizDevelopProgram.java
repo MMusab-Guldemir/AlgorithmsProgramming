2de15b0f912bd27a0aa16ded6b3b11b466e1ff8c
Muhammed <mmusabgldmr@gmail.com>
2026-01-20 11:33:39 +0300

    .
    .

---
+++ src/InteractiveMathQuizDevelopProgram.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+import java.lang.Math;
+
+
+public class InteractiveMathQuizDevelopProgram {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        System.out.println("Random number: " + random.nextInt(1, 11));
+    }
+}
